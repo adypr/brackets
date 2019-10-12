350bb5efd8e4e37d4af5bd03cc80ccd59c94e81d
Adypr <workady2015@gmail.com>
2019-10-12 17:19:52 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
+  let template = /\d/.test(bracketsConfig[0][0]) ? `${bracketsConfig[0][0]}${bracketsConfig[0][1]}` : `\\${bracketsConfig[0][0]}\\${bracketsConfig[0][1]}`;
+  
+  bracketsConfig.forEach(element => {
+    let newTemplate = /\d/.test(element[0]) ? `${element[0]}${element[1]}` : `\\${element[0]}\\${element[1]}`;
+    template = `${template}\|${newTemplate}`;
+  });
+  let tempExpression = new RegExp(template, 'g');
+
+  let resStr = str;
+
+  while (tempExpression.test(resStr) && (resStr !== '')) {
+    resStr = resStr.replace(tempExpression, '');
+  }
+  
+  return resStr === '';
+  
 }
+
